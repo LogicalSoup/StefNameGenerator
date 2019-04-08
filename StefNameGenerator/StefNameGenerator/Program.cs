fdfbd55d1040de3cddcb1633096452655213d255
Thomas Plenderleith <39415356+LogicalSoup@users.noreply.github.com>
2019-04-08 10:11:38 +0100

    Update Program.cs

--- StefNameGenerator/StefNameGenerator/Program.cs
+++ StefNameGenerator/StefNameGenerator/Program.cs
@@ -22,6 +22,7 @@ namespace StefNameGenerator
                 "Chefanie",
                 "Gosh",
                 "Gregg",
+                "Graig",
                 "Mash",
                 "Willian",
                 "Joby",
